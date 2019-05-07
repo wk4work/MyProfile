2914c278c670f7f58b153a1b9a0e3a5936570811
wangkun <wk4work@gmail.com>
2019-05-07 22:59:02 +0800

    Create MyBatisConfig.java

---
+++ SpringBootConfig/MyBatisConfig.java
@@ -0,0 +1,14 @@
+@org.springframework.context.annotation.Configuration
+public class MyBatisConfig {
+
+    @Bean
+    public ConfigurationCustomizer configurationCustomizer(){
+        return new ConfigurationCustomizer(){
+
+            @Override
+            public void customize(Configuration configuration) {
+                configuration.setMapUnderscoreToCamelCase(true);
+            }
+        };
+    }
+}
